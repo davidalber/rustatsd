227eff21f697fc084f18aef912776959c29ced7d
David Alber <alber.david@gmail.com>
2018-06-09 20:29:39 -0700

    Add configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
+extern crate config;
+
 use std::net::UdpSocket;
 use std::process;
 
+struct Config {
+    port: u32,
+    flush_interval: u32,
+}
+
+impl Config {
+    fn new(conf: config::Config) -> Config {
+        let port = conf.get_int("port").unwrap_or(8125) as u32;
+        let flush_interval = conf.get_int("flushInterval").unwrap_or(102) as u32;
+        Config { port, flush_interval }
+    }
+}
+
+fn load_config() -> Config {
+    let mut config = config::Config::default();
+    config
+        .merge(config::File::with_name("Config")).unwrap();
+    Config::new(config)
+}
+
 fn main() {
-    let socket = match UdpSocket::bind("127.0.0.1:6000") {
-        Ok(socket) => socket,
+    let config = load_config();
+    let socket = match UdpSocket::bind(format!("127.0.0.1:{}", config.port)) {
+        Ok(socket) => {
+            println!("Listening on port {}...", config.port);
+            socket
+        },
         Err(_err) => {
             eprintln!("Could not bind to socket.");
             process::exit(1);
