aedd28626f34b3f8a52b5a8f783271e09f2836be
David Alber <alber.david@gmail.com>
2018-09-22 23:49:59 -0700

    Hashing metrics into worker buckets

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,24 @@
 extern crate config;
+extern crate seahash;
 
 use std::net::UdpSocket;
 use std::process;
+use seahash::hash;
 
 const BUFFER_SIZE: usize = 576;
-const BUFFER_WINDOW_COUNT: usize = 5;
 
 struct Config {
     port: u32,
     flush_interval: u32,
+    worker_count: u8,
 }
 
 impl Config {
     fn new(conf: config::Config) -> Config {
         let port = conf.get_int("port").unwrap_or(8125) as u32;
         let flush_interval = conf.get_int("flushInterval").unwrap_or(102) as u32;
-        Config { port, flush_interval }
-    }
-}
-
-struct BufferWindowManager {
-    buf: [[u8; BUFFER_SIZE]; BUFFER_WINDOW_COUNT],
-    buf_ready: [bool; BUFFER_SIZE],
-    buf_window: usize,
-}
-
-impl BufferWindowManager {
-    fn new() -> BufferWindowManager {
-        BufferWindowManager {
-            buf: [[0; BUFFER_SIZE]; BUFFER_WINDOW_COUNT],
-            buf_ready: [true; BUFFER_SIZE],
-            buf_window: BUFFER_WINDOW_COUNT-1,
-        }
-    }
-
-    fn get_next(&mut self) -> usize {
-        let ret = self.buf_window;
-        self.buf_window = (self.buf_window + 1) % BUFFER_WINDOW_COUNT;
-        self.buf_ready[ret] = false;
-        ret
+        let worker_count = conf.get_int("workerCount").unwrap_or(4) as u8;
+        Config { port, flush_interval, worker_count }
     }
 }
 
@@ -49,9 +29,8 @@ fn load_config() -> Config {
     Config::new(config)
 }
 
-fn main() {
-    let config = load_config();
-    let socket = match UdpSocket::bind(format!("127.0.0.1:{}", config.port)) {
+fn make_socket(config: &Config) -> UdpSocket {
+    match UdpSocket::bind(format!("127.0.0.1:{}", config.port)) {
         Ok(socket) => {
             println!("Listening on port {}...", config.port);
             socket
@@ -60,14 +39,32 @@ fn main() {
             eprintln!("Could not bind to socket.");
             process::exit(1);
         },
-    };
-    let mut buf_window_manager = BufferWindowManager::new();
+    }
+}
+
+fn get_stat_worker(stat_name: &str, config: &Config) -> u8 {
+    (hash(stat_name.as_bytes()) % config.worker_count as u64) as u8
+}
+
+fn process_stats(buf: &[u8], config: &Config) {
+    let buf_cow = String::from_utf8_lossy(buf);
+    for stat in buf_cow.split("\n") {
+        if let Some(stat_name) = stat.split(":").nth(0) {
+            println!("[ {} ] {:?}", get_stat_worker(stat_name, &config), stat);
+        }
+    }
+}
+
+fn main() {
+    let config = load_config();
+    let mut buf = [0; BUFFER_SIZE];
+    let socket = make_socket(&config);
 
     loop {
-        let buf_window = buf_window_manager.get_next();
-        match socket.recv_from(&mut buf_window_manager.buf[buf_window]) {
+        match socket.recv_from(&mut buf) {
             Ok((amt, _src)) => {
-                println!("{:?}", String::from_utf8_lossy(&buf_window_manager.buf[buf_window][..amt]));
+                let buf = &buf[..amt];
+                process_stats(buf, &config);
             },
             Err(_err) => (),
         };
