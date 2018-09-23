c2a6ecc69cef13d1a6fba635b51729e69965d099
David Alber <alber.david@gmail.com>
2018-09-23 00:03:51 -0700

    Move metric ingestion into struct

--- src/main.rs
+++ src/main.rs
@@ -20,53 +20,65 @@ impl Config {
         let worker_count = conf.get_int("workerCount").unwrap_or(4) as u8;
         Config { port, flush_interval, worker_count }
     }
-}
 
-fn load_config() -> Config {
-    let mut config = config::Config::default();
-    config
-        .merge(config::File::with_name("Config")).unwrap();
-    Config::new(config)
-}
-
-fn make_socket(config: &Config) -> UdpSocket {
-    match UdpSocket::bind(format!("127.0.0.1:{}", config.port)) {
-        Ok(socket) => {
-            println!("Listening on port {}...", config.port);
-            socket
-        },
-        Err(_err) => {
-            eprintln!("Could not bind to socket.");
-            process::exit(1);
-        },
+    fn load_config() -> Config {
+        let mut config = config::Config::default();
+        config
+            .merge(config::File::with_name("Config")).unwrap();
+        Config::new(config)
     }
 }
 
-fn get_stat_worker(stat_name: &str, config: &Config) -> u8 {
-    (hash(stat_name.as_bytes()) % config.worker_count as u64) as u8
+struct MetricIngester {
+    config: Config,
+    socket: UdpSocket,
 }
 
-fn process_stats(buf: &[u8], config: &Config) {
-    let buf_cow = String::from_utf8_lossy(buf);
-    for stat in buf_cow.split("\n") {
-        if let Some(stat_name) = stat.split(":").nth(0) {
-            println!("[ {} ] {:?}", get_stat_worker(stat_name, &config), stat);
+impl MetricIngester {
+    fn new() -> MetricIngester {
+        let config = Config::load_config();
+        let socket = match UdpSocket::bind(format!("127.0.0.1:{}", config.port)) {
+            Ok(socket) => {
+                println!("Listening on port {}...", config.port);
+                socket
+            },
+            Err(_err) => {
+                eprintln!("Could not bind to socket.");
+                process::exit(1);
+            },
+        };
+
+        MetricIngester { config, socket }
+    }
+
+    fn get_stat_worker(&self, stat_name: &str) -> u8 {
+        (hash(stat_name.as_bytes()) % self.config.worker_count as u64) as u8
+    }
+
+    fn process_stats(&self, buf: &[u8]) {
+        let buf_cow = String::from_utf8_lossy(buf);
+        for stat in buf_cow.split("\n") {
+            if let Some(stat_name) = stat.split(":").nth(0) {
+                println!("[ {} ] {:?}", self.get_stat_worker(stat_name), stat);
+            }
+        }
+    }
+
+    fn run(&self) {
+        let mut buf = [0; BUFFER_SIZE];
+        loop {
+            match self.socket.recv_from(&mut buf) {
+                Ok((amt, _src)) => {
+                    let buf = &buf[..amt];
+                    self.process_stats(buf);
+                },
+                Err(_err) => (),
+            };
         }
     }
 }
 
 fn main() {
-    let config = load_config();
-    let mut buf = [0; BUFFER_SIZE];
-    let socket = make_socket(&config);
-
-    loop {
-        match socket.recv_from(&mut buf) {
-            Ok((amt, _src)) => {
-                let buf = &buf[..amt];
-                process_stats(buf, &config);
-            },
-            Err(_err) => (),
-        };
-    }
+    let metric_ingester = MetricIngester::new();
+    metric_ingester.run();
 }
